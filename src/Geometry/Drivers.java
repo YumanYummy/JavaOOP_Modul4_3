aae0ff08e7c13ae4884b6876d43e7442eb4bb39d
YumanYummy <ahmadrizki0209@gmail.com>
2022-06-03 00:16:10 +0700

    Modul4_3 : Adding More Syntax and Adding Geometry Method

--- src/Geometry/Drivers.java
+++ src/Geometry/Drivers.java
@@ -7,21 +7,32 @@ public class Drivers {
     static Lingkaran L  = new Lingkaran();
     static Tabung T = new Tabung();
     static Persegi P = new Persegi();
+    static Limas S = new Limas();
 
     public static void Input(){
-        int Jari,Long,Side ;
+        int Jari,Long,Side,Alass, TinggiAlas ;
         Scanner g = new Scanner(System.in);
+
         System.out.print("Input Jari-Jari: ");
         Jari = g.nextInt();
         L.setJariJari(Jari);
+
         System.out.print("Input Tinggi: ");
         Long = g.nextInt();
         T.setJariJari(Jari);
         T.setTinggi(Long);
+
         System.out.print("Input Sisi: ");
         Side = g.nextInt();
         P.setSisi(Side);
 
+        System.out.print("Input Alas: ");
+        Alass = g.nextInt();
+        System.out.print("Input Tinggi Alas: ");
+        TinggiAlas = g.nextInt();
+        S.setTinggi(Long);
+        S.setAlas(Alass);
+        S.setTinggiAlas(TinggiAlas);
     }
 
     public static void Lingkaran(){
@@ -44,11 +55,18 @@ public class Drivers {
         System.out.println();
     }
 
+    public static void Limas(){
+        System.out.println();
+        System.out.println("=== Limas ===");
+        System.out.printf("Volume Limas Adalah: %.1f\n",S.getVolume());
+    }
+
     public static void main(String[] args) {
         System.out.println("====== Welcome ======");
         Input();
         Lingkaran();
         Tabung();
         Persegi();
+        Limas();
     }
 }
--- src/Geometry/Limas.java
+++ src/Geometry/Limas.java
@@ -1,4 +1,23 @@
 package Geometry;
 
-public class Limas {
+
+public class Limas extends Persegi{
+    int Alas, Tinggi, TinggiAlas;
+    double Volume;
+
+    @Override
+    public double getVolume() {
+        return Volume = 0.3333*(0.5*Alas*TinggiAlas)*Tinggi;
+    }
+
+    public void setAlas(int Alas) {
+        this.Alas = Alas;
+    }
+    public void setTinggi(int Tinggi){
+        this.Tinggi = Tinggi;
+    }
+
+    public void setTinggiAlas(int TinggiALas){
+        this.TinggiAlas = TinggiALas;
+    }
 }
